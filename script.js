3c7ae97cf134fc470aa8c7d360c3e0c8ed8f1a2e
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2022-08-16 22:14:08 -0300

    Projeto concluído.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let b7Validator = {
+let B7Validator = {
   handleSubmit: (event) => {
     event.preventDefault();
-
     let send = true;
-    let inputs = form.querySelectorAll('.input');
+
+    let inputs = form.querySelectorAll('input');
+
+    B7Validator.clearErrors();
+
     for (let i = 0; i < inputs.length; i++) {
       let input = inputs[i];
-      let check = b7Validator.checkInput(input);
-
+      let check = B7Validator.checkInput(input);
       if (check !== true) {
         send = false;
-        b7Validator.showError(input, check);
+        B7Validator.showError(input, check);
       }
     }
 
@@ -32,20 +34,48 @@ let b7Validator = {
             }
             break;
           case 'min':
+            if (input.value.length < rDetails[1]) {
+              return (
+                'Campo tem que ter pelo menos ' + rDetails[1] + ' caractes'
+              );
+            }
+            break;
+          case 'email':
+            if (input.value != '') {
+              let regex =
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              if (!regex.test(input.value.toLowerCase())) {
+                return 'E-mail digitado não é válido!';
+              }
+            }
             break;
         }
       }
     }
+
     return true;
   },
   showError: (input, error) => {
     input.style.borderColor = '#FF0000';
+
     let errorElement = document.createElement('div');
     errorElement.classList.add('error');
     errorElement.innerHTML = error;
 
-    input.insertBefore(errorElement);
+    input.parentElement.insertBefore(errorElement, input.ElementSibling);
+  },
+  clearErrors: () => {
+    let inputs = form.querySelectorAll('input');
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].style = '';
+    }
+
+    let errorElements = document.querySelectorAll('.error');
+    for (let i = 0; i < errorElements.length; i++) {
+      errorElements[i].remove();
+    }
   },
 };
+
 let form = document.querySelector('.b7validator');
-form.addEventListener('submit', b7Validator.handleSubmit);
+form.addEventListener('submit', B7Validator.handleSubmit);
